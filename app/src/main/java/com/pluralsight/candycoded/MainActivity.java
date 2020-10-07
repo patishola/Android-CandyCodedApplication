b6aeb6bdfab2cb516ab5fc3bb7560ab5eafe6974
patishola <72296653+patishola@users.noreply.github.com>
2020-10-07 23:21:47 +0100

    successful

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -86,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionItemSelected(MenuItem item) {
-        Intent infoIntent = new Intent( packageContext: this, InfoActivity.class);
+        Intent infoIntent = new Intent(this, InfoActivity.class);
         startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
     }
